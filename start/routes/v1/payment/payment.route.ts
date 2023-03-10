107512fb37f04e548c70425f2c0464b010c05233
oluwakayode <kaylove4real10@gmail.com>
2023-03-10 15:38:04 +0100

    init commit

--- start/routes/v1/payment/payment.route.ts
+++ start/routes/v1/payment/payment.route.ts
@@ -1,11 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.post('/initialize', 'PaymentsController.initialize')
-  Route.get('/verify-transaction', 'PaymentsController.verifyTransaction')
-  Route.get('/banks', 'PaymentsController.getBanks')
-  Route.post('/verify-bank', 'PaymentsController.verifyBankDetail')
+  Route.post('/initialize', 'PaymentsController.initialize').middleware(['auth'])
+  Route.get('/verify-transaction', 'PaymentsController.verifyTransaction').middleware(['auth'])
+  Route.get('/banks', 'PaymentsController.getBanks').middleware(['auth'])
+  Route.post('/verify-bank', 'PaymentsController.verifyBankDetail').middleware(['auth'])
+  Route.post('/webhook', 'PaymentsController.webhookEvent')
 })
   .prefix('/api/v1/payment')
-  .middleware(['auth'])
   .namespace('App/Controllers/Http')
